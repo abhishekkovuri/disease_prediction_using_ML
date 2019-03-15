134cf2b8f9dd80ca08cf30210bcd594978263230
KOVURI ABHISHEK <36876451+abhishekkovuri@users.noreply.github.com>
2019-03-15 12:43:24 +0530

    Update GUI.py
    removed extra code

--- Disease-prediction-using-Machine-Learning-master/GUI.py
+++ Disease-prediction-using-Machine-Learning-master/GUI.py
@@ -135,9 +135,6 @@ if __name__=='__main__':
     w2 = Label(root, justify=CENTER, text="Disease Predictor using Machine Learning", fg="black", bg="skyblue")
     w2.config(font=("Elephant", 30))
     w2.grid(row=1, column=0, columnspan=2, padx=100)
-    # w2 = Label(root, justify=LEFT, text="A Project by VAMA", fg="black", bg="skyblue")
-    # w2.config(font=("Aharoni", 30))
-    # w2.grid(row=2, column=0, columnspan=2, padx=100)
 
     # labels
     NameLb = Label(root, text="Patient's Name", fg="black", bg="skyblue")
